72eb5b027612e1e31789185c4e7dabcf934d94d1
isnotvinicius <vinicius.oliveira3301@gmail.com>
2024-01-03 18:22:54 -0300

    Header component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,9 @@
-<script>import "../app.pcss";</script><h1>Layout</h1>
-<ul>
-    <li><a href="/">Home</a></li>
-    <li><a href="/sobre">Sobre</a></li>
-    <li><a href="/portfolio">Portfólio</a></li>
-    <li><a href="/contato">Contato</a></li>
-    <li><a href="/faq">FAQ</a></li>
-</ul>
+<script>
+  import Header from "$lib/Header.svelte";
+  import "../app.pcss";
+</script>
 
-<slot></slot>
+<Header />
+
+<slot />
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,4 @@
 <script>
-  import Teste from "$lib/Teste.svelte";
 </script>
 
 <h1>Aprendendo Svelte</h1>
-
-<Teste />
